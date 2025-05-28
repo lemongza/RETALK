f7ce875d4e1c7f4c61a0c69cf112649a5d3ebe25
zelatndnjs <zelatndnjs@naver.com>
2025-05-28 10:42:47 +0900

    메인 페이지 1+2 생성

--- frontend/src/pages/Home/MainPage.jsx
+++ frontend/src/pages/Home/MainPage.jsx
@@ -4,17 +4,13 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   width: 100vw;
-  height: 100vh;
-  position: relative;
+  min-height: 200vh; /* 상하 합친 높이 */
   background: #052210;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  position: relative;
 `;
 
 const Header = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   width: 100%;
   height: 80px;
@@ -22,7 +18,8 @@ const Header = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 0 5vw;
-  box-sizing: border-box;
+  background: #052210;
+  z-index: 10;
 `;
 
 const RetalkLogo = styled.div`
@@ -54,18 +51,67 @@ const LoginButton = styled.div`
   font-size: 1rem;
 `;
 
-const TitleContainer = styled.div`
+const FirstSection = styled.section`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 10vw;
+`;
+
+const TitleBlock = styled.div`
   text-align: right;
   color: white;
   font-family: 'Luckiest Guy';
-  font-size: 5vw;
+  font-size: 6vw;
   line-height: 1.2;
-  margin-top: -25vh;
-  margin-left: 70vh;
+  margin-bottom: 20vh;
 `;
 
 const Highlight = styled.span`
   color: #00C853;
+`;
+
+const SecondSection = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding-top: 0vh;
+  padding-bottom: 20vh;
+  color: white;
+`;
+
+const SubTitle = styled.div`
+  font-family: 'Pretendard';
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+`;
+
+const MainTitle = styled.div`
+  font-family: 'BMHANNA';
+  font-size: 6rem;
+  line-height: 1.3;
+  margin-bottom: 1rem;
+`;
+
+const Description = styled.div`
+  font-family: 'Pretendard';
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #ccc;
+`;
+
+const Footer = styled.footer`
+  font-family: 'Pretendard';
+  font-size: 1rem;
+  color: #d9d9d9;
+  position: absolute;
+  bottom: 20px;
+  left: 68px;
 `;
 
 export default function MainPage() {
@@ -79,11 +125,34 @@ export default function MainPage() {
           <LoginButton>Login</LoginButton>
         </NavGroup>
       </Header>
-      <TitleContainer>
-        <div>INTERACTIVE</div>
-        <div>BOOK CLUB PLATFORM</div>
-        <div><Highlight>READ & TALK</Highlight></div>
-      </TitleContainer>
+
+      <FirstSection>
+        <TitleBlock>
+          <div>INTERACTIVE</div>
+          <div>BOOK CLUB PLATFORM</div>
+          <div><Highlight>READ & TALK</Highlight></div>
+        </TitleBlock>
+      </FirstSection>
+
+      <SecondSection>
+        <SubTitle>독서에 관심은 많지만<br /> 모임 참여가 부담스러우셨나요?</SubTitle>
+        <MainTitle>
+          “<br />
+          <Highlight>누구나</Highlight> 자유롭게 참여하는<br />
+          <Highlight>독서 토론</Highlight> 플랫폼<br />
+          ”
+        </MainTitle>
+        <Description>
+          독서 모임에 참여하기 위해 금전적 부담을 느끼는 분들을 위해,<br />
+          누구나 책을 읽고 자유롭게 의견을 나눌 수 있는<br />
+          비용 없는 커뮤니티 플랫폼을 만들었습니다.
+        </Description>
+      </SecondSection>
+
+      <Footer>
+        <div><b>Github 🔗</b></div>
+        <div>Developer @김민정 @장수원 @정혜영 @조은지 @최지우 @홍영준</div>
+      </Footer>
     </Wrapper>
   );
 }
